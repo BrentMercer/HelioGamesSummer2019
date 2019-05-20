635cf56286356e2c6809e6152c5d4f07e31c7024
Brent <hicx76@hotmail.com>
2019-05-20 14:02:24 -0600

    Basic scripting lesson

---
+++ GameOne/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    //Jump
+    //Move forward and back
+    //Flip on Y
+
+}
