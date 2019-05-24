bc175246b1d4c2124bef13b4b0248056f1d0f395
SpencerChugg <sdchugg@gmail.com>
2019-05-24 16:28:58 -0600

    Changes

---
+++ GameOne/Assets/Scripts/GameActionObjHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionObjHandler : MonoBehaviour
+{
+    public GameActionObj ActionObj;
+    public UnityEvent Event;
+
+    void Start()
+    {
+        ActionObj.Action += OnRaise;
+    }
+    private void OnRaise ()
+    {
+        Event.Invoke();
+    }
+}
