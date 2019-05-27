aede07a8b7f755e742201f9ae581279444df5cce
Brent <hicx76@hotmail.com>
2019-05-27 16:22:26 -0600

    Added tileset and player.

---
+++ Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float runSpeed = 5f;
+
+    Rigidbody2D myRigidbody;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
+        float horizontalControls = Input.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(horizontalControls * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1);
+        }
+    }
+}
