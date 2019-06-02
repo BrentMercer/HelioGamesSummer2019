42fd789a01563c3308cc0f372fee6343f41aed3d
Brent <hicx76@hotmail.com>
2019-06-02 12:49:59 -0600

    New environment art and tilemaps

---
+++ Platformer/Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMover : MonoBehaviour
+{
+    public CharacterController2D controller;
+
+    private float horizontalMove = 0f;
+    public float runSpeed = 40f;
+
+    bool jump = false;
+    bool crouch = false;
+
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
